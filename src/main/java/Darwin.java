cdb1c0e7c66fa935fb8d4ab60722da13f3c8bcf7
emperorgaodi <aarongaoweijie030103@gmail.com>
2026-01-22 22:41:01 +0800

    edit level 1 Echo

--- src/main/java/Darwin.java
+++ src/main/java/Darwin.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Darwin {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Darwin");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            System.out.println("____________________________________________________________");
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
