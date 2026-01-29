f5492ec1ec0b54258b756082b740edc3bf3ad79b
emperorgaodi <aarongaoweijie030103@gmail.com>
2026-01-30 01:36:59 +0800

    edit level 8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,26 @@
+import java.time.LocalDate;
+
 public class Deadline extends Task {
 
     protected String by;
+    protected LocalDate date;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, String by) throws DarwinException {
         super(description);
         this.by = by;
+        this.date = DateParser.parseDate(by);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        String displayDate = DateParser.formatDateForDisplay(date);
+        // Print in MMM dd yyyy format
+        return "[D]" + super.toString() + " (by: " + displayDate + ")";
     }
 
     @Override
     public String toFileFormat() {
-        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
+        // Save in yyyy-mm-dd format
+        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + date.toString();
     }
 }
